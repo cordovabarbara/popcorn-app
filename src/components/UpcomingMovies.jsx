2c9e7feb5868288b28334c4955bd781b6dc6172f
cordovabarbara <aliendocordova@gmail.com>
2024-03-04 21:45:07 -0500

    add search

--- src/components/UpcomingMovies.jsx
+++ src/components/UpcomingMovies.jsx
@@ -1,26 +1,35 @@
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import useFetch from '../hooks/useFetch';
 import '../styles/styles.scss'
 
 const UpcomingMovies = () => {
   const { upComingMovie, getMovies } = useFetch();
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-    getMovies();
-  }, []); // Llamada una vez al montar el componente
+    getMovies(searchTerm);
+  }, [setSearchTerm]); // 
 
   return (
     <div className='upcomingmovie_container'>
+      <div className='search'>
+      <input
+        type="text"
+        placeholder="Buscar películas..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      </div>
       <h1>Próximas Películas</h1>
       {upComingMovie.map((movie) =>( 
         <li key={movie.id}>
           <h2>{movie.title}</h2>
-          <p>{movie.overview}</p>
           <img
           src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
           alt={movie.title}>
           </img>
+          <p>{movie.overview}</p>
         </li>
       ))}
     </div>
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,16 +3,18 @@ import axios from "axios";
 
 const useFetch = () => {
 
+    // VARIABLES DE ESTADO
 const [upComingMovie, setUpComingMovie] = useState([])
 const [hasError, setHasError] = useState(false)
 
 const urlUpcoming = 'https://api.themoviedb.org/3/movie/upcoming';
 const API_KEY ='9acc36a0b8d6390b09f1c8d0109bce32';
 
-const getMovies = () => {
+const getMovies = (searchTerm = "") => {
     axios.get(urlUpcoming, {
         params: {
             api_key: API_KEY,
+            query: searchTerm,
         },})
         .then(res => {
             setUpComingMovie(res.data.results);
@@ -20,7 +22,7 @@ const getMovies = () => {
             setHasError(false);
         })
         .catch(err => {
-            setHasError(true); // Aquí estableces hasError en true en caso de error
+            setHasError(true); 
             console.error('Error fetching data:', err);
         });
 }
